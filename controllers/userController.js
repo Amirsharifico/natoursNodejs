4a825ff57cf668b9d6f509dd478e0481c75e2558
amirsharifico <amirsharifico@gmail.com>
2023-09-06 23:01:51 -0400

    implimented router and controller

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'To be defined ',
+  });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'To be defined ',
+  });
+};
+exports.getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'To be defined ',
+  });
+};
+exports.updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'To be defined ',
+  });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'To be defined ',
+  });
+};
