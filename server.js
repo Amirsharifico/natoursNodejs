74e1ec83042566305af3d3a0f491ca24b3f9aa86
amirsharifico <amirsharifico@gmail.com>
2023-09-12 14:22:10 -0400

    after connecting to MangoDB , created TourSchema and moedl

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
@@ -17,10 +18,32 @@ mongoose
     console.log('DB connection successful!');
   });
 
-const app = require('./app');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, required: [true, ' A toutr must have a price'] },
+});
 
-// console.log(process.env)
+const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 💥:', err);
+  });
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
