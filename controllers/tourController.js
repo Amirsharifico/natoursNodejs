68a7a4caf53bab228fa977918069efe498afd94b
amirsharifico <amirsharifico@gmail.com>
2023-09-12 21:12:38 -0400

    applied try catch and DB Tour.create on createTour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,4 @@
-// const Tour = require('../models/tourModel');
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'faile',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
@@ -34,13 +24,22 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    // data: {
-    //   tour: newTour,
-    // },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'failed',
+      message: 'Invalid data sent',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
