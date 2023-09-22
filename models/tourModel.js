52715fbb427d9c44d97aeae2bb730c791224ecca
amirsharifico <amirsharifico@gmail.com>
2023-09-21 22:02:08 -0400

    updated Tour Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,9 +5,47 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: { type: Number, default: 4.5 },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: { type: Number, default: 4.5 },
+  retingQuantity: {
+    type: Number,
+    default: 0,
+  },
   price: { type: Number, required: [true, ' A toutr must have a price'] },
+  PriceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    required: [true, 'A tour must have a description'],
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
