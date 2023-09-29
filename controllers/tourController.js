9f2ca50c10453455ffe2233e39e98db098afa0fc
amirsharifico <amirsharifico@gmail.com>
2023-09-29 15:11:47 -0400

    applied orice snd rating sorting as well as default soring based on createdAt

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,14 +3,22 @@ const Tour = require('../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     //build the query
-    console.log(req.query);
+    //1-A Advanced filtering
+
     const queryObject = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObject[el]);
+    //1-B Advanced filtering
     let queryStr = JSON.stringify(queryObject);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+    //2 Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
     // execute the query
     const tours = await query;
     // send response
