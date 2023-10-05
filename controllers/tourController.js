75141f41fd1ad1a6033774194fe0d960f75645e7
amirsharifico <amirsharifico@gmail.com>
2023-10-04 23:29:25 -0400

    limited some fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -19,6 +19,14 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort('-createdAt');
     }
+
+    //3 Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
     // execute the query
     const tours = await query;
     // send response
