1e3ca36f4e497420f2beb196652615ae0daeaf48
amirsharifico <amirsharifico@gmail.com>
2023-10-19 23:58:46 -0400

    added slug document middleware in mangodb

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -58,6 +61,25 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+//document middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// eslint-disable-next-line prefer-arrow-callback
+// tourSchema.pre('save', function (next) {
+//   // eslint-disable-next-line no-console
+//   console.log('will save the document...');
+//   next();
+// });
+// // eslint-disable-next-line prefer-arrow-callback
+// tourSchema.post('save', function (doc, next) {
+//   // eslint-disable-next-line no-console
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
